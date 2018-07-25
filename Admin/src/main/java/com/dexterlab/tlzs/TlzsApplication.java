3926e7689c6ec9addacb166b65d58b0e0603be82
tlzs <tjimu@tlzs.co.jp>
2018-07-25 23:11:51 +0900

    init

---
+++ Admin/src/main/java/com/dexterlab/tlzs/TlzsApplication.java
@@ -0,0 +1,12 @@
+package com.dexterlab.tlzs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TlzsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TlzsApplication.class, args);
+	}
+}
